75768c0d54576a2ce714f26bc677fdabc21af9a4
Sharath <44522660+Sharath-Ramachandran@users.noreply.github.com>
2020-04-16 18:25:57 +0530

    Added broadcast to order to all robots from station
    The broadcast of order from station to robot was made after I gave detach() and added a bool value to make sure that all the data was transferred from initiator.
    Also, gave a while(1) at the end so that the code never stops in both the station and robot code. 
    Need to add locks to make the code better.
    Submitted the code, just to make sure all the relevant needed code changes are present, howsoever tiny

--- robot.cpp
+++ robot.cpp
@@ -37,158 +37,164 @@ Function to create server thread
 */
 void createServer() {
 
-    int portNo;
+	int portNo;
 
-    portNo = generatePortNo();
-    myDetails.networkInfo.portNo = portNo;
+	portNo = generatePortNo();
+	myDetails.networkInfo.portNo = portNo;
 
-    int server_fd, new_socket;
-    int opt = 1;
-    int recv_msg;
+	int server_fd, new_socket;
+	int opt = 1;
+	int recv_msg;
 
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
 
-    // create socket descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-      perror("robot.cpp:createServer() -> socket descriptor failed\n");
-      exit(EXIT_FAILURE);
-    }
+	// create socket descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("robot.cpp:createServer() -> socket descriptor failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    // socket settings
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-      perror("robot.cpp:createServer() -> setsockopt failed\n");
-      exit(EXIT_FAILURE);
-    }
+	// socket settings
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+		perror("robot.cpp:createServer() -> setsockopt failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    // create socket with port number as PortNo
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(portNo);
-
-
-    // bind socket with the ipv4 address and port no
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-      perror("robot.cpp:createServer() -> bind failed\n");
-      exit(EXIT_FAILURE);
-    }
+	// create socket with port number as PortNo
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(portNo);
 
 
-    // listen to port
-    if (listen(server_fd, 1000) < 0) {
-      perror("robot.cpp:createServer() -> listen failed\n");
-      exit(EXIT_FAILURE);
-    }
+	// bind socket with the ipv4 address and port no
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		perror("robot.cpp:createServer() -> bind failed\n");
+		exit(EXIT_FAILURE);
+	}
 
 
-    thread th1(connectToInitiator);
-    th1.detach();
+	// listen to port
+	if (listen(server_fd, 1000) < 0) {
+		perror("robot.cpp:createServer() -> listen failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    while(1) {
 
-      // accept new connection
-      if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        perror("station.cpp:createServer() -> accept failed\n");
-        exit(EXIT_FAILURE);
-      }
+	thread th1(connectToInitiator);
+	th1.detach();
 
-      int msg;
-      msg = -1;
-      send(new_socket, &msg, sizeof(int), 0);
-      cout << "Sent Welcome Message\n";
+	while (1) {
 
-      close(new_socket);
-    }
+		// accept new connection
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+			perror("station.cpp:createServer() -> accept failed\n");
+			exit(EXIT_FAILURE);
+		}
+
+		int msg;
+		msg = -1;
+		read(new_socket, &msg, sizeof(msg));
+		if (msg == STATION) {
+			cout << "Got order from station" << endl;
+			Order order;
+			read(new_socket, &order, sizeof(Order));
+			cout << "Station id is: " << order.stationId << endl;
+		}
+		// send(new_socket, &msg, sizeof(int), 0);
+		// cout << "Sent Welcome Message\n";
+
+		close(new_socket);
+	}
 }
 
 
 
 void connectToInitiator() {
 
-  int sock;
-  struct sockaddr_in serv_addr;
-  int initiatorPortNo = 9000;
+	int sock;
+	struct sockaddr_in serv_addr;
+	int initiatorPortNo = 9000;
 
-  int recv_msg;
-  int msg;
+	int recv_msg;
+	int msg;
 
-  // create socket descriptor
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("station.cpp:connectToInitiator() -> socket descriptor failed\n");
-    exit(EXIT_FAILURE);
-  }
-
-
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(initiatorPortNo);
+	// create socket descriptor
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("station.cpp:connectToInitiator() -> socket descriptor failed\n");
+		exit(EXIT_FAILURE);
+	}
 
 
-  // convert ipv4 address from text to binary info
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-    perror("station.cpp:connectToInitiator() -> invalid address not supported\n");
-    exit(EXIT_FAILURE);
-  }
-
-  // connect to server 127.0.0.1:9000
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("station.cpp:connectToInitiator() -> connection failed\n");
-    exit(EXIT_FAILURE);
-  }
-
-  // send initial message about being robot
-  msg = ROBOT;
-
-  // match-1
-  send(sock, &msg, sizeof(int), 0);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(initiatorPortNo);
 
 
-  // receive robot id from robot
-  // match-2
-  recv_msg = read(sock, &myDetails.robotId, sizeof(int));
-  // printRobotInfo(&myDetails, 1);
+	// convert ipv4 address from text to binary info
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		perror("station.cpp:connectToInitiator() -> invalid address not supported\n");
+		exit(EXIT_FAILURE);
+	}
 
-  printMsg("robot.cpp:connectToInitiator() -> received robot id " + to_string(myDetails.robotId));
+	// connect to server 127.0.0.1:9000
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		perror("station.cpp:connectToInitiator() -> connection failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-  // send myDetails i.e. struct Robot
-  // match-3
-  send(sock, &myDetails.networkInfo.portNo, sizeof(int), 0);
-  recv_msg = read(sock, &myDetails, sizeof(Robot));
-  printRobotInfo(&myDetails, 1);
+	// send initial message about being robot
+	msg = ROBOT;
 
-  // receive broadcast message from initiator about number of robots, stations
-  // match-4
-  recv_msg = read(sock, &robot_count, sizeof(int));
-  recv_msg = read(sock, &station_count, sizeof(int));
-  recv_msg = read(sock, &item_count, sizeof(int));
-  recv_msg = read(sock, &grid_size, sizeof(int));
-
-  robots = (Robot *)malloc(robot_count*sizeof(Robot));
-  stations = (Station *)malloc(station_count*sizeof(Station));
-  items = (Item *)malloc(item_count*sizeof(Item));
-
-  // receive broadcast message from initiator about other robot, station information
-  // match-5
-  printMsg("At match-5 step begin");
-  recv_msg = read(sock, robots, robot_count*sizeof(Robot));
-  recv_msg = read(sock, stations, station_count*sizeof(Station));
-  recv_msg = read(sock, items, item_count*sizeof(Item));
-  printMsg("At match-5 step end");
-
-  printMsg(to_string(myDetails.robotId) + ":robot.cpp:connectToInitiator() -> receive broadcast information from initiator");
-
-  printRobotInfo(robots, robot_count);
-  printStationInfo(stations, station_count);
-  printItemInfo(items, item_count);
+	// match-1
+	send(sock, &msg, sizeof(int), 0);
 
 
-  initializeGrid();
-  cout << "here\n";
-  printGrid();
+	// receive robot id from robot
+	// match-2
+	recv_msg = read(sock, &myDetails.robotId, sizeof(int));
+	// printRobotInfo(&myDetails, 1);
 
-  close(sock);
+	printMsg("robot.cpp:connectToInitiator() -> received robot id " + to_string(myDetails.robotId));
+
+	// send myDetails i.e. struct Robot
+	// match-3
+	send(sock, &myDetails.networkInfo.portNo, sizeof(int), 0);
+	recv_msg = read(sock, &myDetails, sizeof(Robot));
+	printRobotInfo(&myDetails, 1);
+
+	// receive broadcast message from initiator about number of robots, stations
+	// match-4
+	recv_msg = read(sock, &robot_count, sizeof(int));
+	recv_msg = read(sock, &station_count, sizeof(int));
+	recv_msg = read(sock, &item_count, sizeof(int));
+	recv_msg = read(sock, &grid_size, sizeof(int));
+
+	robots = (Robot *)malloc(robot_count * sizeof(Robot));
+	stations = (Station *)malloc(station_count * sizeof(Station));
+	items = (Item *)malloc(item_count * sizeof(Item));
+
+	// receive broadcast message from initiator about other robot, station information
+	// match-5
+	printMsg("At match-5 step begin");
+	recv_msg = read(sock, robots, robot_count * sizeof(Robot));
+	recv_msg = read(sock, stations, station_count * sizeof(Station));
+	recv_msg = read(sock, items, item_count * sizeof(Item));
+	printMsg("At match-5 step end");
+
+	printMsg(to_string(myDetails.robotId) + ":robot.cpp:connectToInitiator() -> receive broadcast information from initiator");
+
+	printRobotInfo(robots, robot_count);
+	printStationInfo(stations, station_count);
+	printItemInfo(items, item_count);
+
+
+	initializeGrid();
+	cout << "here\n";
+	printGrid();
+
+	close(sock);
 
 }
-
 
 /*
 Function to get details (pre-defined) about the picking station
@@ -219,14 +225,14 @@ Function to get details (pre-defined) about the picking station
 
 int main() {
 
-  // get robot id provided from station
+	// get robot id provided from station
 
-  // myDetails.state = PASSIVE;
+	// myDetails.state = PASSIVE;
 
-  thread th1(createServer);
-  th1.join();
+	thread th1(createServer);
+	th1.join();
 
 
 
-  return 0;
+	return 0;
 }
--- station.cpp
+++ station.cpp
@@ -18,6 +18,7 @@ using namespace std;
 
 // Global Variables
 struct Station myDetails;
+struct Order order;
 int robot_count;
 int station_count;
 int item_count;
@@ -26,13 +27,13 @@ Station* stations;
 Item* items;
 int grid_size;
 int** grid;
+bool isDone = false;
 
 
 // Function Prototypes
 void createServer();
 void connectToInitiator();
-
-
+void BroadCastOrderInfoToRobots(Order order);
 
 
 /*
@@ -41,165 +42,203 @@ It is server listening port
 */
 void createServer() {
 
-  int portNo;
+	int portNo;
 
-  portNo = generatePortNo();
-  myDetails.networkInfo.portNo = portNo;
+	portNo = generatePortNo();
+	myDetails.networkInfo.portNo = portNo;
 
 //  printMsg("station.cpp:createServer() -> Listening on Port " + to_string(portNo));
 
-  int server_fd, new_socket;
-  int opt = 1;
-  int recv_msg;
+	int server_fd, new_socket;
+	int opt = 1;
+	int recv_msg;
 
-  struct sockaddr_in address;
-  int addrlen = sizeof(address);
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
 
-  // create socket description
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-    perror("station.cpp:createServer() -> socket descriptor failed\n");
-    exit(EXIT_FAILURE);
-  }
-
-
-  // socket settings
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-    perror("station.cpp:createServer() -> setsockopt failed\n");
-    exit(EXIT_FAILURE);
-  }
+	// create socket description
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("station.cpp:createServer() -> socket descriptor failed\n");
+		exit(EXIT_FAILURE);
+	}
 
 
-  // create socket with port number as PortNo
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(portNo);
-
-  // bind socket with the ipv4 address and port no
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror("station.cpp:createServer() -> bind failed\n");
-    exit(EXIT_FAILURE);
-  }
+	// socket settings
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+		perror("station.cpp:createServer() -> setsockopt failed\n");
+		exit(EXIT_FAILURE);
+	}
 
 
-  // listen to port
-  if (listen(server_fd, 1000) < 0) {
-    perror("station.cpp:createServer() -> listen failed\n");
-    exit(EXIT_FAILURE);
-  }
+	// create socket with port number as PortNo
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(portNo);
 
-  // printStationInfo(&myDetails);
-  thread th1(connectToInitiator);
-  th1.detach();
+	// bind socket with the ipv4 address and port no
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		perror("station.cpp:createServer() -> bind failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-  while(1) {
 
-    // accept new connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-      perror("station.cpp:createServer() -> accept failed\n");
-      exit(EXIT_FAILURE);
-    }
+	// listen to port
+	if (listen(server_fd, 1000) < 0) {
+		perror("station.cpp:createServer() -> listen failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    int msg;
-    recv_msg = read(new_socket, &msg, sizeof(msg));
-    if (msg == 1) {
-      cout << "New Station is Connected\n";
-    }
-    else {
-      cout << "New Robot is Connected\n";
-    }
-    msg = -1;
-    send(new_socket, &msg, sizeof(int), 0);
-    cout << "Sent Welcome Message\n";
+	// printStationInfo(&myDetails);
+	thread th1(connectToInitiator);
+	th1.detach();
+	while (1) {
 
-    close(new_socket);
-  }
+		// accept new connection
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+			perror("station.cpp:createServer() -> accept failed\n");
+			exit(EXIT_FAILURE);
+		}
+
+		int msg;
+		recv_msg = read(new_socket, &msg, sizeof(msg));
+		if (msg == 1) {
+			cout << "New Station is Connected\n";
+		} else {
+			cout << "New Robot is Connected\n";
+		}
+		msg = -1;
+		send(new_socket, &msg, sizeof(int), 0);
+		cout << "Sent Welcome Message\n";
+
+		close(new_socket);
+	}
 }
 
+int connectToRobot(int robotPortNo) {
+	int sock;
+	struct sockaddr_in serv_addr;
+	int recv_msg;
+	int msg;
 
+	// create socket descriptor
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("station.cpp:connectToRobot() -> socket descriptor failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(robotPortNo);
+
+	// convert ipv4 address from text to binary info
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		perror("station.cpp:connectToRobot() -> invalid address not supported\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// connect to server 127.0.0.1:robotPortNo
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		perror("station.cpp:connectToRobot() -> connection failed\n");
+		exit(EXIT_FAILURE);
+	}
+	return sock;
+}
 
 void connectToInitiator() {
 
-  int sock;
-  struct sockaddr_in serv_addr;
-  int initiatorPortNo = 9000;
+	int sock;
+	struct sockaddr_in serv_addr;
+	int initiatorPortNo = 9000;
 
-  int recv_msg;
-  int msg;
+	int recv_msg;
+	int msg;
 
-  // create socket descriptor
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("station.cpp:connectToInitiator() -> socket descriptor failed\n");
-    exit(EXIT_FAILURE);
-  }
+	// create socket descriptor
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("station.cpp:connectToInitiator() -> socket descriptor failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(initiatorPortNo);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(initiatorPortNo);
 
-  // convert ipv4 address from text to binary info
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-    perror("station.cpp:connectToInitiator() -> invalid address not supported\n");
-    exit(EXIT_FAILURE);
-  }
+	// convert ipv4 address from text to binary info
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		perror("station.cpp:connectToInitiator() -> invalid address not supported\n");
+		exit(EXIT_FAILURE);
+	}
 
-  // connect to server 127.0.0.1:9000
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-    perror("station.cpp:connectToInitiator() -> connection failed\n");
-    exit(EXIT_FAILURE);
-  }
+	// connect to server 127.0.0.1:9000
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		perror("station.cpp:connectToInitiator() -> connection failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-  // send initial message about being a station
-  msg = STATION;
-  // match-1
-  send(sock, &msg, sizeof(int), 0);
+	// send initial message about being a station
+	msg = STATION;
+	// match-1
+	send(sock, &msg, sizeof(int), 0);
 
-  // receive station id from station
-  // match-2
-  recv_msg = read(sock, &myDetails.stationId, sizeof(int));
-  // printStationInfo(&myDetails, 1);
-
-
-  // send myDetails i.e. struct Station
-  // match-3
-  send(sock, &myDetails.networkInfo.portNo, sizeof(int), 0);
-  recv_msg = read(sock, &myDetails, sizeof(Station));
-  printStationInfo(&myDetails, 1);
-
-  // receive broadcast message from initiator about number of robots, stations
-  // match-4
-  recv_msg = read(sock, &robot_count, sizeof(int));
-  recv_msg = read(sock, &station_count, sizeof(int));
-  recv_msg = read(sock, &item_count, sizeof(int));
-  recv_msg = read(sock, &grid_size, sizeof(int));
+	// receive station id from station
+	// match-2
+	recv_msg = read(sock, &myDetails.stationId, sizeof(int));
+	// printStationInfo(&myDetails, 1);
 
 
-  robots = (Robot *)malloc(robot_count*sizeof(Robot));
-  stations = (Station *)malloc(station_count*sizeof(Station));
-  items = (Item *)malloc(item_count*sizeof(Item));
+	// send myDetails i.e. struct Station
+	// match-3
+	send(sock, &myDetails.networkInfo.portNo, sizeof(int), 0);
+	recv_msg = read(sock, &myDetails, sizeof(Station));
+	printStationInfo(&myDetails, 1);
 
-  // receive broadcast message from initiator about other robot, station information
-  // match-5
-  printMsg("At match-5 step begin");
-  int val;
-  recv_msg = read(sock, robots, robot_count*sizeof(Robot));
-  recv_msg = read(sock, stations, station_count*sizeof(Station));
-  recv_msg = read(sock, items, item_count*sizeof(Item));
-  printMsg("At match-5 step end");
+	// receive broadcast message from initiator about number of robots, stations
+	// match-4
+	recv_msg = read(sock, &robot_count, sizeof(int));
+	recv_msg = read(sock, &station_count, sizeof(int));
+	recv_msg = read(sock, &item_count, sizeof(int));
+	recv_msg = read(sock, &grid_size, sizeof(int));
 
-  printMsg(to_string(myDetails.stationId) + ":station.cpp:connectToInitiator() -> received broadcast information from initiator");
 
-  printRobotInfo(robots, robot_count);
-  printStationInfo(stations, station_count);
+	robots = (Robot *)malloc(robot_count * sizeof(Robot));
+	stations = (Station *)malloc(station_count * sizeof(Station));
+	items = (Item *)malloc(item_count * sizeof(Item));
 
-  initializeGrid();
-  printGrid();
+	// receive broadcast message from initiator about other robot, station information
+	// match-5
+	printMsg("At match-5 step begin");
+	int val;
+	recv_msg = read(sock, robots, robot_count * sizeof(Robot));
+	recv_msg = read(sock, stations, station_count * sizeof(Station));
+	recv_msg = read(sock, items, item_count * sizeof(Item));
+	printMsg("At match-5 step end");
 
-  close(sock);
+	printMsg(to_string(myDetails.stationId) + ":station.cpp:connectToInitiator() -> received broadcast information from initiator");
 
+	printRobotInfo(robots, robot_count);
+	printStationInfo(stations, station_count);
+
+	initializeGrid();
+	printGrid();
+
+	close(sock);
+
+	isDone = true;
 }
 
 
 
+void BroadCastOrderInfoToRobots(Order order) {
+	int i;
+	int msg = STATION;
+	for (i = 0; i < robot_count; i++) {
+		int sock = connectToRobot(robots[i].networkInfo.portNo);
+		cout << "Connected to Robot" << endl;
+		send(sock, &msg, sizeof(int), 0);
+		send(sock, &order, sizeof(Order), 0);
+		close(sock);
+		printMsg("station.cpp:BroadCastOrderInfoToRobots() -> send broadcast message to robot " + to_string(i));
+	}
 
+}
 
 
 
@@ -224,36 +263,42 @@ void showItems() {
 Dummy Function for Items
 */
 struct Item* insertItems() {
-  struct Item items[5];
-  struct Item *tmp;
-  int i;
-  for(i=0;i<5;i++) {
-    tmp = createItem(i+1, 10, i+1,i+1);
-    items[i].itemId = tmp->itemId;
-    items[i].currentCount = tmp->currentCount;
-    items[i].coords = tmp->coords;
-    //placeItemOnGrid(grid, items[i].coords);
-  }
+	struct Item items[5];
+	struct Item *tmp;
+	int i;
+	for (i = 0; i < 5; i++) {
+		tmp = createItem(i + 1, 10, i + 1, i + 1);
+		items[i].itemId = tmp->itemId;
+		items[i].currentCount = tmp->currentCount;
+		items[i].coords = tmp->coords;
+		//placeItemOnGrid(grid, items[i].coords);
+	}
 }
 
 
 int main() {
 
-  srand(time(0));
+	srand(time(0));
 //  myDetails.stationId = generateStationId();
 //  cout << "My Station ID: " << myDetails.stationId << "\n";
 
 //  insertItems();
+	isDone = false;
 
+	myDetails.orderQueue = 10;
+	myDetails.exitQueue = 10;
 
-  myDetails.orderQueue = 10;
-  myDetails.exitQueue = 10;
+	thread th1(createServer);
+	th1.detach();
 
-  thread th1(createServer);
-  th1.join();
+	while (!isDone);
 
-//  struct Item items[5];
+	Order order;
+	order.stationId = myDetails.stationId;
+	BroadCastOrderInfoToRobots(order);
 
-  return 0;
+	// th1.join();
+	while (1);
+	return 0;
 
 }
